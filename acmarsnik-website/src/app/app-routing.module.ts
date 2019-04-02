80f7d8a2fe64733485d528d17bec5afab3ecf931
Albert Marsnik <albertmarsnik@gmail.com>
2019-04-02 23:47:53 +0200

    changed logo, to something that is easier to have readable writing on top of.  Added bootstrap & flexbox, set up initial navbar, set up home, about and apps components with just the generic message in there, added routes for each of those components

--- acmarsnik-website/src/app/app-routing.module.ts
+++ acmarsnik-website/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AppsComponent } from './apps/apps.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  {
+      path: 'about',
+      component: AboutComponent
+  },
+  {
+      path: 'apps',
+      component: AppsComponent
+  },
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- acmarsnik-website/src/app/app.module.ts
+++ acmarsnik-website/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AppsComponent } from './apps/apps.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AppsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
