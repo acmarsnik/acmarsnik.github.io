b75adfb65a1f2f98b795eff516639c5412b9efba
Albert Marsnik <albertmarsnik@gmail.com>
2019-04-16 06:01:43 +0200

    moved toolbar into own navigation/header component, slid links to right of navbar

--- acmarsnik-website/src/app/app.module.ts
+++ acmarsnik-website/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatTabsModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +9,7 @@ import { AboutComponent } from './about/about.component';
 import { AppsComponent } from './apps/apps.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { BlogComponent } from './blog/blog.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 @NgModule({
   declarations: [
@@ -16,16 +17,23 @@ import { BlogComponent } from './blog/blog.component';
     HomeComponent,
     AboutComponent,
     AppsComponent,
-    BlogComponent
+    BlogComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
-    MatButtonModule, 
+    MatButtonModule,
     MatCheckboxModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTabsModule
+  ],
+  exports: [
+    MatTabsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
